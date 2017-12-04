5d1ebe395c4dadc4711394cb69e5d63ac2b802ba
dennisyu <yuhaokui@rebind.cn>
2017-12-04 19:07:05 +0800

    导航改为tabbar

--- App.js
+++ App.js
@@ -6,14 +6,20 @@
 
 import React, { Component } from 'react'
 import {
-  StackNavigator
+  StackNavigator, TabNavigator
 } from 'react-navigation'
 import MainScreen from './Views/Main'
 import ProfileScreen from './Views/Profile'
 
-const MainApp = StackNavigator({
-  Main: {screen: MainScreen},
-  Profile: {screen: ProfileScreen}
+// const MainApp = StackNavigator({
+//   Main: {screen: MainScreen},
+//   Profile: {screen: ProfileScreen}
+// })
+const MainApp = TabNavigator({
+    Main: {screen: MainScreen},
+    Profiel: {screen: ProfileScreen}
+},{
+    animationEnabled: true
 })
 
 export default class App extends React.Component {
